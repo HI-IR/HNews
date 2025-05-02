66597f6447bd17fece41aaccdaa36d02143f9539
HI-IR <15314828+hi-ir@user.noreply.gitee.com>
2025-05-02 23:47:47 +0800

    完成RV

---
+++ app/src/main/java/com/assessment/zhihunes/customview/BottomToolBar.kt
@@ -0,0 +1,10 @@
+package com.assessment.zhihunes.customview
+
+/**
+ * description ： 底部工具栏
+ * author : HI-IR
+ * email : qq2420226433@outlook.com
+ * date : 2025/5/2 23:36
+ */
+class BottomToolBar {
+}
---
+++ app/src/main/java/com/assessment/zhihunes/view/DetailActivity.kt
@@ -0,0 +1,37 @@
+package com.assessment.zhihunes.view
+
+import android.app.PendingIntent.getActivity
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.assessment.zhihunes.databinding.ActivityHomeBinding
+
+
+class DetailActivity : AppCompatActivity() {
+    private val binding: ActivityHomeBinding by lazy {
+        ActivityHomeBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        initWindow()
+    }
+
+    private fun initWindow() {
+        // 设置状态栏透明
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = window
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+}
