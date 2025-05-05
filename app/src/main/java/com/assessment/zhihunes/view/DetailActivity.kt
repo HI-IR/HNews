5dec300fb0b440c6e9094d0184edf824d288d788
HI-IR <15314828+hi-ir@user.noreply.gitee.com>
2025-05-05 17:19:53 +0800

    完成了分享功能

--- app/src/main/java/com/assessment/zhihunes/view/DetailActivity.kt
+++ app/src/main/java/com/assessment/zhihunes/view/DetailActivity.kt
@@ -5,22 +5,23 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.MotionEvent
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.assessment.zhihunes.adapter.DetailAdapter
+import com.assessment.zhihunes.customview.CustomShare
 import com.assessment.zhihunes.databinding.ActivityDetailBinding
+import com.assessment.zhihunes.utils.CopyUtils
 import com.assessment.zhihunes.utils.DateUtils
+import com.assessment.zhihunes.utils.ShareUtils
 import com.assessment.zhihunes.viewmodel.NewDetailViewModel
 
 
 class DetailActivity : AppCompatActivity() {
     private var date: String = ""
     private var id: Long = -1
-    private var disableSwipe = false
     private val binding: ActivityDetailBinding by lazy {
         ActivityDetailBinding.inflate(layoutInflater)
     }
@@ -31,7 +32,6 @@ class DetailActivity : AppCompatActivity() {
 
     companion object {
         fun startActivity(context: Context, id: Long, date: String) {
-            //TODO("带实现跳转")
             val intent = Intent(context, DetailActivity::class.java)
             intent.putExtra("id", id)
             intent.putExtra("date", date)
@@ -56,14 +56,70 @@ class DetailActivity : AppCompatActivity() {
 
     @SuppressLint("ClickableViewAccessibility")
     private fun initEvent() {
-        var startX = 0f
-        var isInteracting = false
+
         binding.bottomtoolbar.setOnBackClickListener { finish() }
         binding.bottomtoolbar.setOnShareClickListener {
             //TODO(分享等待实现))
+            val customShare = CustomShare(this)
+
+            // 设置不同分享渠道的点击监听器
+            customShare.setOnQqClickListener {
+                var link:String = ""
+                //寻找到当前页的链接
+                viewModel.netDataList.value!!.stories.forEach {
+                    if (it.id.toLong() == id){
+                       link = it.url
+                    }
+                }
+                ShareUtils.shareToQQ(this,link)
+                customShare.dismiss()
+            }
+
+            customShare.setOnWxClickListener {
+                var link:String = ""
+                //寻找到当前页的链接
+                viewModel.netDataList.value!!.stories.forEach {
+                    if (it.id.toLong() == id){
+                        link = it.url
+                    }
+                }
+                ShareUtils.shareToWX(this,link)
+                customShare.dismiss()
+            }
+
+            customShare.setOnLinkClickListener {
+                //寻找到当前页的链接，并复制
+                viewModel.netDataList.value!!.stories.forEach {
+                    if (it.id.toLong() == id){
+                        CopyUtils.copy(this@DetailActivity ,it.url)
+                    }
+                }
+                Toast.makeText(this, "已复制链接", Toast.LENGTH_SHORT).show()
+
+                customShare.dismiss()
+            }
+
+            customShare.setOnBrowseClickListener {
+                var link:String = ""
+                //寻找到当前页的链接
+                viewModel.netDataList.value!!.stories.forEach {
+                    if (it.id.toLong() == id){
+                        link = it.url
+                    }
+                }
+                ShareUtils.shareToBrowser(this,link)
+                customShare.dismiss()
+            }
+
+
+
+            // 显示对话框
+            customShare.show()
+
         }
         binding.bottomtoolbar.setOnCommentsClickListener {
-            CommentsActivity.startActivity(this,id,viewModel.currentExtraStory.value!!.comments)
+
+            CommentsActivity.startActivity(this, id, viewModel.currentExtraStory.value!!.comments)
         }
         binding.bottomtoolbar.setOnPopularityClickListener {
             //TODO(点赞)
@@ -72,24 +128,6 @@ class DetailActivity : AppCompatActivity() {
             //TODO(收藏)
         }
 
-
-
-        binding.vp2.getChildAt(0).setOnTouchListener { _, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    startX = event.x
-                    isInteracting = false
-                }
-                MotionEvent.ACTION_MOVE -> {
-                    val deltaX = event.x - startX
-                    if (!isInteracting && deltaX > 20 && binding.vp2.currentItem == 1 && disableSwipe) {
-                        isInteracting = true
-                        return@setOnTouchListener true
-                    }
-                }
-            }
-            false // 不拦截其他滑动（如左滑）
-        }
 
 
 
@@ -109,7 +147,7 @@ class DetailActivity : AppCompatActivity() {
                     viewModel.loadPrevDay()
                 }
 
-                if(position!=0 && position!= viewModel.adapterDataList.value!!.size -1){
+                if (position != 0 && position != viewModel.adapterDataList.value!!.size - 1) {
                     id = viewModel.adapterDataList.value!![position]!!.id
                     viewModel.getExtraStory(viewModel.adapterDataList.value!![position]!!.id)
                 }
@@ -131,7 +169,7 @@ class DetailActivity : AppCompatActivity() {
                     binding.vp2.adapter = adapter
                     adapter!!.notifyItemRangeChanged(1, viewModel.netDataList.value!!.stories.size)
                     viewModel.adapterDataList.value!!.forEachIndexed { index, storyBefore ->
-                        if (index!=0 && index!= viewModel.adapterDataList.value!!.size-1){
+                        if (index != 0 && index != viewModel.adapterDataList.value!!.size - 1) {
                             if (storyBefore!!.id == id) binding.vp2.setCurrentItem(index, false)
                         }
                     }
@@ -151,10 +189,10 @@ class DetailActivity : AppCompatActivity() {
         viewModel.isLoadingNext.observe(this) {
             if (!it) {
                 adapter!!.notifyItemRangeChanged(1, viewModel.netDataList.value!!.stories.size)
-                if (viewModel.netDataList.value!!.date == DateUtils.formateDate(DateUtils.getCurrentDate())){
-                    Toast.makeText(this,"没有更多咯",Toast.LENGTH_SHORT).show()
+                if (viewModel.netDataList.value!!.date == DateUtils.formateDate(DateUtils.getCurrentDate())) {
+                    Toast.makeText(this, "没有更多咯", Toast.LENGTH_SHORT).show()
                     viewModel.loadPrevDay()
-                }else{
+                } else {
                     binding.vp2.setCurrentItem(viewModel.adapterDataList.value!!.size - 2, false)
 
                 }
